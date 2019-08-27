071b7e3aa8a9cb7c9f16c87598f8182cdd4fde4d
wendyle09 <54556660+wendyle09@users.noreply.github.com>
2019-08-26 19:21:41 -0700

    Add files via upload

---
+++ Items.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace SimpleInventory
+{
+   class Items
+   {
+      private int _ID;
+      private string _descript;
+      private double _cost;
+      private int _quantity;
+
+      // constructor
+      public Items(int intID, string strDescript, double dblCost, int intQuantity)
+      {
+         _ID = intID;
+         _descript = strDescript;
+         _cost = dblCost;
+         _quantity = intQuantity;
+      }
+
+      // properties
+      public int ID
+      {
+         get
+         {
+            return _ID;
+         }
+      }
+
+      public string Description
+      {
+         get
+         {
+            return _descript;
+         }
+      }
+
+      public double Cost
+      {
+         get
+         {
+            return _cost;
+         }
+      }
+
+      public int Quantity
+      {
+         get
+         {
+            return _quantity;
+         }
+      }
+   }
+}
